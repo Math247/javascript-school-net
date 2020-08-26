bbf2878e1f1f40013af20ae3e2897d70f72402ef
Matheus Carregal <matheuschabib@gmail.com>
2020-08-26 13:37:49 -0300

    Update main.js Condições

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
-console.log("Hello World");
+/*console.log("Hello World");
 //quando usamos aspas, se torna um caractere, e vira uma string;
 var number = 0;
 console.log(typeof number);
 number = "0"
 console.log(typeof number);
+*/
+var age = prompt("How old are you?");
+/*if(age >= 18){
+    console.log("You are authorized");
+}else{
+    console.log("You are not authorized");
+}
+*/
+switch (true){
+    case (age >= 18):
+        console.log("You are authorized switch");
+    break
+    case (age < 18):
+        console.log("You are not authorized switch");
+    break
+    default:
+        console.log("default function");
+}
