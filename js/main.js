2f80fb4b3c4b4cc838d964e4a7d4b0fdcfe624fb
Matheus Carregal <matheuschabib@gmail.com>
2020-08-26 13:29:43 -0300

    Update main.js var

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 console.log("Hello World");
+//quando usamos aspas, se torna um caractere, e vira uma string;
+var number = 0;
+console.log(typeof number);
+number = "0"
+console.log(typeof number);
