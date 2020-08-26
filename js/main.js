53a640d4cc663a585bfa270c0c73e3cc34d84ecb
Matheus Carregal <matheuschabib@gmail.com>
2020-08-26 13:50:16 -0300

    Update main.js Loops

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-/*console.log("Hello World");
+console.log("Hello World");
 //quando usamos aspas, se torna um caractere, e vira uma string;
 var number = 0;
 console.log(typeof number);
 number = "0"
 console.log(typeof number);
-*/
+
 var age = prompt("How old are you?");
-/*if(age >= 18){
+if(age >= 18){
     console.log("You are authorized");
 }else{
     console.log("You are not authorized");
 }
-*/
+
 switch (true){
     case (age >= 18):
         console.log("You are authorized switch");
@@ -22,3 +22,18 @@ switch (true){
     default:
         console.log("default function");
 }
+
+var i = 0;
+while (i < 10){
+    console.log(i);
+    i++
+}
+
+for (var a = 0; a < 10; a++){
+    console.log(a);
+}
+
+[0, 1, 2, 3, 4, 5, 6].forEach(function(value, key){
+    console.log(value, key);
+});
+
