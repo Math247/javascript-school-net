b47e81988bba7fd24e2a982bf2b8f57a4a818651
Matheus Carregal <matheuschabib@gmail.com>
2020-08-26 14:55:25 -0300

    Update main.js DOM Html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
 var click_me = document.getElementById("click_me");
+var input = document.getElementsByClassName("number_val_input");
+var btn_generate = document.getElementById("generate_btn");
+var body1 = document.getElementsByTagName('body');
+
+var select = document.createElement('select');
+
+body1[0].appendChild(select);
+for(var i = 0; i < 10; i++){
+    var option = document.createElement('option');
+    option.id = '#id_' + i;
+    option.value = i;
+    option.innerHTML = i;
+    select.appendChild(option);
+}
+
+btn_generate.addEventListener('click', generate);
+
+function generate(){
+    var value = input[0].value;
+    for(var i = 0; i < value; i++){
+        var int = document.createElement('input');
+        int.id = "btn_" + i;
+    
+        var body = document.getElementsByTagName('body');
+        body[0].appendChild(int);
+    }
+}
+
 click_me.addEventListener("click", fnClickMe);
 function fnClickMe(){
     alert("You clicked me");
@@ -13,8 +41,8 @@ function validar(){
     }
 }
 
-function over(){
-    console.log("over me");
+function over(elemento){
+    elemento.innerHTML = "Over me";
 }
 
 /*console.log("Hello World");
